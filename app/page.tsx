114fa077d85bf0d62e9f79d86dc2df8ca7ec90e2
One-Vision-Soft <onevisionsoft@gmail.com>
2024-08-01 20:34:44 +0600

    new file and folders

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      
+      <center>
+      <button className="mt-6 mb-6 p-2 ring-offset-2 ring-2 ring-orange-500 text-white bg-orange-500 shadow rounded">
+        machineLab
+      </button>
+        
+        
+
+      <h1>Upgrading</h1>
+      <p>It may Take few days</p>
+      <hr></hr>
+      <br></br>
+      <br></br>
+      <h3>Contact</h3>
+      <p>M / W: +8801749771939</p>
+      <p>E : manik@machinelab.com.bd</p>
+      </center>
+      
+    </main>
+  );
+}
